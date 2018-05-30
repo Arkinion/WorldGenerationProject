546401614b6615cda3608c8de36ca8b518bc40d1
Arkinion <nicholasgr44@gmail.com>
2018-05-30 15:50:37 -0500

    Fixed the line-changing. They should now be able to change correctly.

--- WorldGenerationProject/src/main/java/com/mycompany/worldgenerationproject/GUI/Math/SiteAlgorithm/VoronoiDiagram.java
+++ WorldGenerationProject/src/main/java/com/mycompany/worldgenerationproject/GUI/Math/SiteAlgorithm/VoronoiDiagram.java
@@ -42,60 +42,76 @@ public class VoronoiDiagram
     
     
     
-    
+    // Adds a new Cell to the diagram
     public void add(Cell c)
     {
+        // Runs for each cell that already exists
         for (Cell cell : cells)
         {
-
+            
+            // The vector from the new cell's site to the current cell's site
             Vector2 between = Vector2.sub(cell.getSite(), c.getSite());
-            Vector2 mid = Vector2.add(cell.getSite(), between.div(2));
+            // Finds the midpoint between the two cells
+            Vector2 mid = Vector2.add(c.getSite(), between.div(2));
+            
+            // The points that exist on each side of the new edge
+            ArrayList<Vector2> startSide = new ArrayList<Vector2>();
+            ArrayList<Vector2> endSide = new ArrayList<Vector2>();
 
+            // For all of the edges on the cell being looked at
             for (Line e : cell.getEdges())
             {
-                // MUST FIX
-                ArrayList<Vector2> startSide = new ArrayList<Vector2>();
-                ArrayList<Vector2> endSide = new ArrayList<Vector2>();
+                // The vector from the start to the site
+                Vector2 siteVec = Vector2.sub(cell.getSite(), e.getStart()).normalize();
+                // The vector from the start to the midpoint
+                Vector2 midVec = Vector2.sub (mid           , e.getStart()).normalize();
+                // The vector from the start to the end
+                Vector2 edgeVec = Vector2.add(e.getEnd()    , e.getStart()).normalize();
                 
-                Vector2 siteVec = Vector2.add(e.getA(), cell.getSite()).normalize();
-                Vector2 midVec = Vector2.add(e.getA(), mid).normalize();
-                Vector2 edgeVec = Vector2.add(e.getA(), e.asVector()).normalize();
-
+                // The cross product of the site vector as compared to the edge vector
                 double spatialSite = edgeVec.cross(siteVec);
+                // The cross product of the midpoint vector as opposed to the edge vector
                 double spatialMid = edgeVec.cross(midVec);
                 
+                // The line that is perpendicular to the line between the new site and current site
                 Line perLine = Line.perLine( new Line(mid, between.normalize()) );
+                // Bounding this line into the diagram
                 perLine.bound(bounds);
                 
+                // If both of the cross-products are on the same side
                 if (spatialSite < 0 && spatialMid < 0)
                 {
+                    // If the midpoint vector is between the edge vector and
+                    // the site vector
                     if (spatialSite <= spatialMid)
                     {
+                        // Finds the intersection of the perpendicular line and this edge
                         Vector2 intersection = perLine.intersect(e);
                         
+                        // If the intersection exists (i.e. they aren't parallel)
                         if (intersection != null)
                         {
-                            boolean isStart = true;
-                            Vector2 start = e.getStart();
-                            Vector2 end = e.getEnd();
+                            // FIX (NOT ALWAYS CLOSEST, USE SIDE OF MIDPOINT LIKE IN NEW EDGE)
+                            boolean isStart;
                             
-                            if (isStart)
+                            Vector2 midpointToEStart = Vector2.sub(e.getStart(), e.getA());
+                            Vector2 midpointToInter  = Vector2.sub(intersection, e.getA());
+                            if (compareDirections(midpointToEStart, midpointToInter))
                             {
-                                if (intersection.dist(cell.getSite()) < start.dist(cell.getSite()))
-                                {
+                                if (intersection.dist(cell.getSite()) < e.getStart().dist(cell.getSite()))
                                     e.setStart(intersection);
-                                }
                             }
                             else
                             {
-                                if (intersection.dist(cell.getSite()) < end.dist(cell.getSite()))
-                                {
+                                if (intersection.dist(cell.getSite()) < e.getEnd().dist(cell.getSite()))
                                     e.setEnd(intersection);
-                                }
                             }
                             
-                            Vector2 sourceToStart = Vector2.sub(e.getStart(), e.getA());
-                            Vector2 sourceToInter = Vector2.sub(intersection, e.getA());
+                            Vector2 start = e.getStart();
+                            Vector2 end = e.getEnd();
+                            
+                            Vector2 sourceToStart = Vector2.sub(perLine.getStart(), perLine.getA());
+                            Vector2 sourceToInter = Vector2.sub(intersection, perLine.getA());
                             
                             if (compareDirections(sourceToStart, sourceToInter))
                             {
@@ -105,12 +121,14 @@ public class VoronoiDiagram
                             {
                                 endSide.add(intersection);
                             }
-                            
                         }
                     }
                 }
+                // If both of the cross-products are on the same side
                 else if (spatialSite >= 0 && spatialMid >= 0)
                 {
+                    // If the midpoint vector is between the edge vector and
+                    // the site vector
                     if (spatialSite >= spatialMid)
                     {
                         Vector2 intersection = perLine.intersect(e);
@@ -133,6 +151,19 @@ public class VoronoiDiagram
                             {
                                 if (intersection.dist(cell.getSite()) < end.dist(cell.getSite()))
                                     e.setEnd(intersection);
+                            }
+                            
+                            
+                            Vector2 sourceToStart = Vector2.sub(e.getStart(), e.getA());
+                            Vector2 sourceToInter = Vector2.sub(intersection, e.getA());
+                            
+                            if (compareDirections(sourceToStart, sourceToInter))
+                            {
+                                startSide.add(intersection);
+                            }
+                            else
+                            {
+                                endSide.add(intersection);
                             }
                         }
                     }
